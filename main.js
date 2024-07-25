620c38265152d4642eb8262937c64d82b7e5082c
JakePaul11 <mumukuku346@gmail.com>
2024-07-25 12:32:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
